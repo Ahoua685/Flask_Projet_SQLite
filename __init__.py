984a341bc24866aaba3848c41f79f40c7a369f5e
Ahoua685 <ahouacool@gmail.com>
2025-01-16 10:36:46 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -76,6 +76,9 @@ def enregistrer_client():
     conn.commit()
     conn.close()
     return redirect('/consultation/')  # Rediriger vers la page d'accueil après l'enregistrement
+@app.route('/fiche_nom')
+def fiche_nom():
+    return render_template('fiche.html')
                                                                                                                                        
 if __name__ == "__main__":
   app.run(debug=True)
