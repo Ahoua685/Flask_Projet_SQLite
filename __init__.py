80b403b2bf343dfe9de4b886c65d53a0ea408f35
Ahoua685 <ahouacool@gmail.com>
2025-01-16 11:58:21 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -78,8 +78,7 @@ def enregistrer_client():
     return redirect('/consultation/')  # Rediriger vers la page d'accueil après l'enregistrement
 @app.route('/fiche_nom/<string:nom>')
 def Readfiche_nom(nom): 
-     if not est_authentifie():
-        
+    if not est_authentifie():    
     conn = sqlite3.connect('database.db')
     cursor = conn.cursor()
     cursor.execute('SELECT * FROM clients WHERE nom LIKE ?', ('%' + nom + '%',))
